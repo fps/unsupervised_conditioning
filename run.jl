9c4b46867218b8f2b30fbc30f63ac647f6d532cc
Florian Paul Schmidt <florian_paul.schmidt@uni-bielefeld.de>
2024-04-26 15:07:01 +0200

    needs a simpler latent model still

--- run.jl
+++ run.jl
@@ -1,10 +1,14 @@
+import Pkg
+Pkg.activate(".")
+
 import Plots
 import Flux
 import Statistics
 
 t = Float32.(repeat(0:(1/1000):1, 1, 1, 1))
 x = sin.(100 .* t)
-y = tanh.(10 .* t .* sin.(100 .* t))
+y = tanh.(5 .* t .* sin.(100 .* t))
+
 latent = zeros(Float32, size(t)...)
 
 width = 8
@@ -19,12 +23,12 @@ opt = Flux.setup(Flux.Adam(), [model, latent])
 
 loss(m, x, y) = Flux.Losses.mse(m(x), y)
 
-for m in 1:100;
+for m in 1:1000;
     display(m);
-    for n in 1:100; 
+    for n in 1:200; 
         the_loss, the_grads = Flux.withgradient([model, latent]) do params
             # display(model2)
-            loss(params[1], cat(x, params[2], dims=2), y) + Statistics.mean(diff(params[2], dims=1).^2) + Statistics.mean(params[2]).^2
+            loss(params[1], cat(x, params[2], dims=2), y) + Statistics.mean(diff(params[2], dims=1).^2) + Statistics.mean(params[2]).^2 + (1 - Statistics.var(params[2])).^2
         end
         
         Flux.update!(opt, [model, latent], the_grads[1])
@@ -32,7 +36,10 @@ for m in 1:100;
     end; 
     
     Plots.plot(
-        Plots.plot(model(cat(x, latent, dims=2))[:,1,1]),
-        Plots.plot(latent[:,1,1]), layout=(2,1)
+        Plots.plot(x[:,1,1], title="Training input", legend=:none), 
+        Plots.plot(y[:,1,1], title="Training onput", legend=:none), 
+        Plots.plot(model(cat(x, latent, dims=2))[:,1,1], title="Model output", legend=:none),
+        Plots.plot(latent[:,1,1], title="Latent", legend=:none), 
+        layout=(4,1)
     ) |> display
 end
