e6aa769d45fa5454fef39af3c1d8f9286df5ad38
Florian Paul Schmidt <florian_paul.schmidt@uni-bielefeld.de>
2025-02-17 14:39:24 +0100

    - success: bandlimited radial basis functions

--- run_audio.jl
+++ run_audio.jl
@@ -40,41 +40,59 @@ y = tanh.(control2 .+ control1 .* x)
 y = Float32.(repeat(y, 1, 1, 1)) |> g
 x = Float32.(repeat(x, 1, 1, 1)) |> g
 
+sigmas = [1/30, 1/10]
+
 num_basis_functions = 80
 # basis = Float32.(cat([[sin.(f .* t .+ phase) for t in t, f in 2*pi .* (1:num_basis_functions)] for phase in [0, pi/2]]..., dims=2))
 # basis = cat(basis, ones(1001,1), (1/1000).*(0:(1/1000):1), dims=2)
 # basis = cat(ones(1001,1), 0:(1/1000):1, dims=2)
-basis = Float32.([exp.(-num_basis_functions.^2 .* (1 .* (t.-center)).^2) for t in t, center in 0:(1/(num_basis_functions-1)):1]) |> g
-basis = basis ./ sum(basis, dims=2)
-    
+# basis = Float32.([exp.(-num_basis_functions.^2 .* (1 .* (t.-center)).^2) for t in t, center in 0:(1/(num_basis_functions-1)):1]) |> g
+# basis = basis ./ sum(basis, dims=2)
+basis = Float32.([exp.(-(t.-center).^2 ./ sigma^2) for t in t, center in 0:(1/(num_basis_functions-1)):1, sigma in sigmas]) |> g
+for n in 1:size(basis, 3)
+    basis[:,:,n] = basis[:,:,n] ./ sum(basis[:,:,n], dims=2)
+end
+  
 # basis = basis./sum(basis, dims=2)
     
-num_latent_variables = 2
+num_latent_variables = length(max_frequencies)
 
 I = zeros(num_latent_variables, num_latent_variables)
 for n in 1:num_latent_variables
     I[n,n] = 1
 end
     
-latent_params = 0.00001f0 * randn(Float32, size(basis, 2), num_latent_variables) |> g
+latent_params = 0.00000001f0 * randn(Float32, size(basis, 2), num_latent_variables) |> g
 
 # latent = zeros(Float32, size(t)...)
 
 println("Setting up model...")
 
-width = 16
+width = 4
 
 model = Flux.Chain(
     Flux.Conv((1,), (1+num_latent_variables)=>width), 
-    [Flux.Conv((1,), width=>width, Flux.selu) for n in 1:8]...,
+    [Flux.Conv((1,), width=>width, Flux.rrelu) for n in 1:16]...,
     Flux.Conv((1,), width=>1)
 ) |> g
 
-opt = Flux.setup(Flux.AdamW(), [model, latent_params])
+opt = Flux.setup(Flux.AdamW(0.0001), [model, latent_params])
 
 loss(m, x, y) = Flux.Losses.mse(m(x), y)
 
 println("Entering training looop...")
+
+function cov(x)
+    m = [Statistics.mean(x[:,i]) for i in 1:size(x,2)]
+
+    [sum( (x[:,i] .- m[i]) .* (x[:,j] .- m[j])) for i in 1:size(x, 2), j in 1:size(x,2)] ./ (size(x,1) - 1)
+end
+
+eye = zeros(Float32, num_latent_variables, num_latent_variables)
+for i in 1:num_latent_variables
+    eye[i,i] = 1.0f0
+end
+eye = g(eye)
 
 for m in 1:5000;
     losses = []
@@ -82,8 +100,9 @@ for m in 1:5000;
         
         print(".")
         the_loss, the_grads = Flux.withgradient([model, latent_params]) do params
-            latent = basis * params[2]
-            loss(params[1], cat(x, latent, dims=2), y) # + 0.00001f0 * (latent[:,1]' * latent[:,2])^2.0f0 # + 0.00001f0 * Statistics.mean(latent).^2.0f0 # + 0.00000001 * sum((1.0f0 .- Statistics.var(latent, dims=2))).^2# + 0.00001 * sum(abs.(latent)) # #  + sum((I - Statistics.cov(latent_params)).^2)# + 10.0f0 * Statistics.mean(diff(latent, dims=1).^2)# 0.01f0 * Statistics.mean(latent.^2) #  + 1.0f0 * (1.0f0 - Statistics.var(latent)).^2 + Statistics.mean(latent.^2) + 0.1f0*(latent[:,1]' * latent[:,2])^2.0f0 # + 0.1f0 * (1.0f0 - Statistics.var(latent)).^2 + 0.1f0*(latent[:,1]' * latent[:,2])^2.0f0
+            # latent = basis * params[2]
+            latent = hcat([basis[:,:,n] * params[2][:,n] for n in 1:size(latent_params, 2)]...)
+            loss(params[1], cat(x, latent, dims=2), y) # + 0.001f0 * sum((eye .- g(cov(latent))).^2) # + 0.00001f0 * (latent[:,1]' * latent[:,2])^2.0f0 # + 0.00001f0 * Statistics.mean(latent).^2.0f0 # + 0.00000001 * sum((1.0f0 .- Statistics.var(latent, dims=2))).^2# + 0.00001 * sum(abs.(latent)) # #  # + 10.0f0 * Statistics.mean(diff(latent, dims=1).^2)# 0.01f0 * Statistics.mean(latent.^2) #  + 1.0f0 * (1.0f0 - Statistics.var(latent)).^2 + Statistics.mean(latent.^2) + 0.1f0*(latent[:,1]' * latent[:,2])^2.0f0 # + 0.1f0 * (1.0f0 - Statistics.var(latent)).^2 + 0.1f0*(latent[:,1]' * latent[:,2])^2.0f0
             # + sum(([1 0] .- latent[1,:]).^2)
                 
             #LinearAlgebra.norm(Statistics.cor(latent, dims=1) - [1 0; 0 1])
@@ -95,18 +114,21 @@ for m in 1:5000;
     end; 
     display((m, Statistics.mean(losses)))
     
+    latent = hcat([basis[:,:,n] * latent_params[:,n] for n in 1:size(latent_params, 2)]...)
+    
     Plots.plot(
         Plots.plot(x[:,1,1] |> c, title="Training input", legend=:none), 
         # Plots.plot(control1[:,:,1] |> c, title="Control 1", legend=:none),
         # Plots.plot(control2[:,:,1] |> c, title="Control 2", legend=:none),
         Plots.plot(y[:,1,1] |> c, title="Training output", legend=:none),
-        Plots.plot(model(cat(x, basis * latent_params, dims=2))[:,1,1] |> c, title="Model output", legend=:none),
-        [ Plots.plot((basis * latent_params)[:,n,1] |> c, title="Inferred control input $(n)", legend=:none) for n in 1:num_latent_variables]...,
+        Plots.plot(model(cat(x, latent, dims=2))[:,1,1] |> c, title="Model output", legend=:none),
+        [ Plots.plot((latent)[:,n,1] |> c, title="Inferred control input $(n)", legend=:none) for n in 1:num_latent_variables]...,
         layout=(3+num_latent_variables,1),
         ytickfontsize=4,
         xtickfontsize=4,
         titlefontsize=4,
         margin=1Measures.mm,
-        padding=1Measures.mm
+        padding=1Measures.mm,
+        linewidth=0.25
     ) |> display
 end
