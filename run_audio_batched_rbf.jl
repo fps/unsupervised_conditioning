73a9a68655490a8539e4c354cc1b1a8308ff31e9
Florian Paul Schmidt <florian_paul.schmidt@uni-bielefeld.de>
2025-02-21 09:44:21 +0100

    - lalala

--- run_audio_batched_rbf.jl
+++ run_audio_batched_rbf.jl
@@ -14,60 +14,57 @@ import LinearAlgebra
 
 import Random
 
-println("Setting up things...")
+
+println("Initial setup...")
+
+include("basis.jl")
 
 g = Flux.gpu
 c = Flux.cpu
 g = g
 
-freq = 500
+
+println("Creating synthetic training data...")
 
 FS = 48000
 
 T = 2
 
+blocksize = 1000
+
 t = 0:(1/FS):(T-(1/FS))
+
+t_blocks = reshape(t, 1000, :)
 
 N_data = length(t)
 
-x = randn(N_data) |> g
+x = sin.(40.0f0*2.0f0*pi.*t) .* randn(N_data) |> g
 
-# y = tanh.(5 .* sin.(2*pi*3.1 .* t) .* sin.(2*pi*freq .* t)) .+ sin.(2*pi*10.8 .* t) 
-
-control1 = 0.5f0 .* sin.(2*pi*1.1*t.+0.1) .+ 1.0f0 |> g
-control2 = cos.(2*pi*13.9*t) |> g
-
-# y = control1 .* tanh.(x .* 5.0f0 .* control2)
+control1 = 0.5f0 .* sin.(2*pi*2.25*t.+0.1) .+ 1.0f0 |> g
+control2 = cos.(2*pi*6.9*t) |> g
 
 y = tanh.(control2 .+ control1 .* x) |> g
 
-
-function gaussian(center, sigma, t)
-    exp.(-(t.-center).^2 ./ sigma^2)
-end
-
-function mexican_hat(center, sigma, t)
-   (1 .- ((t .- center) ./ sigma).^2) .* exp.(-(t .- center).^2 ./ (2*sigma^2))
-end
-
-
 num_basis_functions = 40 * T
 
-sigmas = [1/40, 1/10]
-basis_functions = [mexican_hat, gaussian]
+sigmas = [1/20, 1/5]
+basis_functions = [mexican_hat, mexican_hat]
 
 num_latent_variables = length(sigmas)
 
-basis = Float32.([f(center, sigma, t) for center in T .* (0:(1/(num_basis_functions-1)):1), (sigma, f) in zip(sigmas, basis_functions), t in t]) |> g
-# for n in 1:size(basis, 2)
-#     basis[:,n,:] = basis[:,n,:] ./ sum(basis[:,n,:], dims=1)
-# end
-latent_params = 0.00000001f0 * randn(Float32, num_latent_variables, size(basis, 1)) |> g
+basis = Float32.([f(center, sigma, t) for (sigma, f) in zip(sigmas, basis_functions), t in t, center in T .* (0:(1/(num_basis_functions-1)):1)]) |> g
+    
+latent_params = 0.00000001f0 * randn(Float32, num_latent_variables, num_basis_functions) |> g
 
-I = zeros(num_latent_variables, num_latent_variables)
-for n in 1:length(num_latent_variables)
-    I[n,n] = 1
-end
+
+println("Dividing data into blocks...")
+
+x_blocks = reshape(x, blocksize, :)
+y_blocks = reshape(y, blocksize, :)
+# basis_blocks = reshape(permutedims(basis,(1, 3, 2)), length(sigmas), blocksize, num_basis_functions, :)
+basis_blocks = cat([basis[:,start:(start+blocksize-1),:] for start in 1:blocksize:96000]..., dims=4)
+# basis_blocks = Float32.(
+
 
 println("Setting up model...")
 
@@ -83,45 +80,11 @@ opt = Flux.setup(Flux.AdamW(0.001), [model, latent_params])
 
 loss(m, x, y) = Flux.Losses.mse(m(x), y)
 
+
 println("Entering training loop...")
 
-function cov(x)
-    m = [Statistics.mean(x[:,i]) for i in 1:size(x,2)]
-
-    [sum( (x[:,i] .- m[i]) .* (x[:,j] .- m[j])) for i in 1:size(x, 2), j in 1:size(x,2)] ./ (size(x,1) - 1)
-end
-
-eye = zeros(Float32, num_latent_variables, num_latent_variables)
-for i in 1:num_latent_variables
-    eye[i,i] = 1.0f0
-end
-eye = g(eye)
-
-function i2r(x)
-    real.(x) .+ imag.(x)
-end
-
 for m in 1:5000;
-    losses = []
-    for n in 1:50; 
-        data_loader = Flux.MLUtils.DataLoader((basis, x, y), batchsize=12000, shuffle=true)
-        print(":")
-        
-        for (basis, x, y) in data_loader
-            print(".")
-            the_loss, the_grads = Flux.withgradient([model, latent_params]) do params
-                latent = hcat([(params[2][n:n,:] * basis[:,n,:])' for n in 1:size(latent_params, 1)]...)
-                loss(params[1], repeat(cat(x, latent, dims=2), 1, 1, 1), y)
-            end
-            push!(losses, the_loss)
-            
-            Flux.update!(opt, [model, latent_params], the_grads[1])
-        end
-    end; 
-    display((m, Statistics.mean(losses)))
-    
-    latent = hcat([(latent_params[n:n,:] * basis[:,n,:])' for n in 1:size(latent_params, 1)]...)
-
+    latent = hcat([(latent_params[n:n,:] * basis[n,:,:]')' for n in 1:size(latent_params, 1)]...)
     
     Plots.plot(
         Plots.plot(x[:,1,1] |> c, title="Training input", legend=:none), 
@@ -134,6 +97,32 @@ for m in 1:5000;
         titlefontsize=4,
         margin=1Measures.mm,
         padding=1Measures.mm,
-        linewidth=0.25
+        linewidth=0.25,
     ) |> display
+
+    losses = []
+    for n in 1:50; 
+        batchsize = 5
+        data_loader = Flux.MLUtils.DataLoader((x_blocks, y_blocks, basis_blocks), batchsize=batchsize, shuffle=true)
+        print("<")
+        
+        for (x_block, y_block, basis_block) in data_loader
+            # print((size(x_block), size(y_block), size(basis_block)))
+            batchsize = size(x_block, 2)
+            print(".")
+            the_loss, the_grads = Flux.withgradient([model, latent_params]) do params
+                local m = params[1]
+                local latent_params = params[2]
+                # latent = hcat([(latent_params[n:n,:] * basis[n,:,:]')' for n in 1:size(latent_params, 1)]...)
+                local latent = cat([cat([basis_block[sigma,:,:,batch] * latent_params[sigma,:] for sigma in 1:length(sigmas)]..., dims=2) for batch in 1:batchsize]..., dims=3)
+                # print((size(latent), size(x_block), size(y_block)))
+                loss(m, cat(reshape(x_block, blocksize, 1, batchsize), latent, dims=2), reshape(y_block, blocksize, 1, batchsize))
+            end
+            push!(losses, the_loss)
+            
+            Flux.update!(opt, [model, latent_params], the_grads[1])
+        end
+        print(">")
+    end; 
+    display((m, Statistics.mean(losses)))
 end
