f4bf7de6b66c6c978718a7d8fca40dd7e33e6644
Florian Paul Schmidt <mista.tapas@gmx.net>
2025-03-01 08:28:37 +0100

    brummer script

---
+++ apply_model.jl
@@ -0,0 +1,22 @@
+import Pkg
+Pkg.activate(".")
+
+import BSON
+import CUDA
+import Flux
+import WAV
+
+c = Flux.cpu
+g = Flux.gpu
+
+BSON.@load "ts9-16x16.bson" model l_limits
+
+model = model |> g
+l_limits = l_limits 
+
+x_test, fs_x_test = WAV.wavread("input.wav")
+
+for l1 in 0:0.1:1;                                                            for l2 in 0:0.1:1;                                                            ll1 = l_limits[1][1]+l1*(l_limits[1][2] - l_limits[1][1]);
+    ll2 = l_limits[2][1] + l2*(l_limits[2][2] - l_limits[2][1]);
+    y_test = model(reshape(cat(0.1*x_test, ll1 * ones(length(x_test), 1), ll2 * ones(length(x_test), 1), dims=2), length(x_test), 3, 1) |> g);              f = open("output-$(l1)-$(l2).wav", "w");                                    WAV.wavwrite(y_test[:,1,1]|>c, f, Fs=48000);                                close(f);                                                                 end;
+end
